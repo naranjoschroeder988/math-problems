e02c3417c67d192189b1de7a08eb9a161d5b53a9
naranjoschroeder988 <naranjoschroeder988@gmail.com>
2025-03-15 16:58:18 +0000

    Add 10.java

---
+++ src/10.java
@@ -0,0 +1,11 @@
+Here is a randomly generated Java program that satisfies your requirements: 
+
+import java.util.Random;
+public class Main {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int x = rand.nextInt(10);
+        int y = rand.nextInt(10);
+        System.out.println("x + y = " + (x + y));
+    }
+}
