40c35cb43a2ff826faa61a0e1129408f5917c2ea
naranjoschroeder988 <naranjoschroeder988@gmail.com>
2025-03-23 00:32:42 +0000

    Add 18.java

---
+++ src/18.java
@@ -0,0 +1,11 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        int c = 7;
+
+        System.out.println("a + b = " + (a + b));
+        System.out.println("b - c = " + (b - c));
+        System.out.println("c / a = " + (c / a));
+    }
+}
