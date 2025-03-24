4f8f783255d1869d33e6db685758a0a3807d60fe
naranjoschroeder988 <naranjoschroeder988@gmail.com>
2025-03-24 01:18:00 +0000

    Add 19.java

---
+++ src/19.java
@@ -0,0 +1,9 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        int number1 = 5;
+        int number2 = 3;
+
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + (number1 + number2));
+        System.out.println("The difference between " + number1 + " and " + number2 + " is: " + (number1 - number2));
+    }
+}
