65809eb3b6b5bf9b7fa87498d0f822441ed48d9f
naranjoschroeder988 <naranjoschroeder988@gmail.com>
2025-04-07 03:07:47 +0000

    Add 25.java

---
+++ src/25.java
@@ -0,0 +1,13 @@
+public class MathProblem {
+    public static void main(String[] args) {
+        // Example Java code to solve a math problem
+
+        int x = 5;
+        int y = 3;
+
+        double result = x + y;
+        System.out.println("The sum of " + x + " and " + y + " is: " + result);
+
+        // Add more examples if needed
+    }
+}
