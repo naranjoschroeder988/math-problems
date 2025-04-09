a3e166fff193b88a05d775d93c2c2c192ab3ee0e
naranjoschroeder988 <naranjoschroeder988@gmail.com>
2025-04-09 04:58:41 +0000

    Add 28.java

---
+++ src/28.java
@@ -0,0 +1,6 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2}, {3, 4}};
+        System.out.println("The product of each row is: " + Arrays.deepToString(matrix));
+    }
+}
