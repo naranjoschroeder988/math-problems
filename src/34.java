a55dd7d482fe7122f8a6756a8d133b73593a09ee
naranjoschroeder988 <naranjoschroeder988@gmail.com>
2025-04-26 04:41:55 +0000

    Add 34.java

---
+++ src/34.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+
+public class MathProblems {
+    public static void main(String[] args) {
+        int x = (int)(Math.random() * 100);
+        System.out.println("Random number: " + x);
+    }
+}
