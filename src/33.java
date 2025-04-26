520b936e60cc5055e403973667e361529b3ee491
naranjoschroeder988 <naranjoschroeder988@gmail.com>
2025-04-26 04:21:02 +0000

    Add 33.java

---
+++ src/33.java
@@ -0,0 +1,10 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+
+        System.out.println("The sum of " + x + " and " + y + " is: " + (x + y));
+        System.out.println("The difference between " + x + " and " + y + " is: " + (x - y));
+        System.out.println("The product of " + x + " and " + y + " is: " + (x * y));
+    }
+}
