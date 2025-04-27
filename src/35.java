1420e1e0603ee706334d0f7726f0a3b23bd72e3c
naranjoschroeder988 <naranjoschroeder988@gmail.com>
2025-04-27 07:26:37 +0000

    Add 35.java

---
+++ src/35.java
@@ -0,0 +1,11 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example Java program with basic mathematical operations
+
+        int result = 2 + 3;
+        System.out.println("The sum of 2 and 3 is: " + result);
+        
+        double area = 5 * 4 / 2;
+        System.out.println("The area of a rectangle with length 5 and width 4 is: " + area);
+    }
+}
