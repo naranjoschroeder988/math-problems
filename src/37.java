439e9a6429f0ce4bc4ccebf74b89df2f7856dc26
naranjoschroeder988 <naranjoschroeder988@gmail.com>
2025-05-18 13:47:34 +0000

    Add 37.java

---
+++ src/37.java
@@ -0,0 +1,10 @@
+public class MathProblemSolver {
+    public static void main(String[] args) {
+        // Example 1: Simple arithmetic problem
+        System.out.println("2 + 3 = " + (2 + 3));
+        
+        // Example 2: Using operator overloading
+        int sum = a + b; // Assuming a and b are integers
+        System.out.println("sum of " + a + " and " + b + " is " + sum);
+    }
+}
