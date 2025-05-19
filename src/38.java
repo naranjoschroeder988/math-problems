3c42fc7def592c826c3e5687fae2ded88f290bd7
naranjoschroeder988 <naranjoschroeder988@gmail.com>
2025-05-19 15:04:27 +0000

    Add 38.java

---
+++ src/38.java
@@ -0,0 +1,7 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        int num = 5;
+        double result = Math.pow(num, 2);
+        System.out.println("The square of " + num + " is: " + result);
+    }
+}
