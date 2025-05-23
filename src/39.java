964c54a2091d30e094c286938e49d3c5e38b5a97
naranjoschroeder988 <naranjoschroeder988@gmail.com>
2025-05-23 07:37:56 +0000

    Add 39.java

---
+++ src/39.java
@@ -0,0 +1,10 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example Java code for mathematical operations
+        int sum = 5 + 3;
+        double product = 7.0 / 2;
+
+        System.out.println("The sum is: " + sum);
+        System.out.println("The product is: " + product);
+    }
+}
