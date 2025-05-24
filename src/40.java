1ebced984ac549e7b2162c4345da94838c9a73ef
naranjoschroeder988 <naranjoschroeder988@gmail.com>
2025-05-24 04:22:37 +0000

    Add 40.java

---
+++ src/40.java
@@ -0,0 +1,13 @@
+public class MathProblemSolver {
+    public static void main(String[] args) {
+        // Example problem 1: Add two numbers
+        int num1 = 5;
+        int num2 = 3;
+        System.out.println("Sum of " + num1 + " and " + num2 + " is " + (num1 + num2));
+        
+        // Example problem 2: Calculate the area of a circle given radius
+        double radius = 7;
+        double area = Math.PI * Math.pow(radius, 2);
+        System.out.println("The area of the circle with radius " + radius + " is " + area);
+    }
+}
