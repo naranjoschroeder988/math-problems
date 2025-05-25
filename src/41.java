13ddfe7ac676a8cea3e80e827c1bd873dd5495c3
naranjoschroeder988 <naranjoschroeder988@gmail.com>
2025-05-25 06:45:10 +0000

    Add 41.java

---
+++ src/41.java
@@ -0,0 +1,9 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        int x = 5;
+        double y = 3.14;
+
+        System.out.println("x is " + x);
+        System.out.println("y is " + y);
+    }
+}
